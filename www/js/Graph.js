9a2b20c43ade34251d0252e666beb099fcc1ee41
Marcel Šulek <marcel@sulek.eu>
2013-08-27 15:31:10 +0200

    prvni nakresleni vsech linii

--- www/js/Graph.js
+++ www/js/Graph.js
@@ -4,7 +4,7 @@
     Graph.displayName = 'Graph';
     var prototype = Graph.prototype, constructor = Graph;
     function Graph(parentSelector, lines){
-      var x$, y$, z$, z1$;
+      var x$, y$, z$, z1$, z2$, z3$, this$ = this;
       this.parentSelector = parentSelector;
       this.lines = lines;
       this.width = 500;
@@ -16,19 +16,39 @@
       x$.attr('width', this.width);
       y$ = this.drawing = this.svg.append('g');
       y$.attr('transform', "translate(" + this.margin_left + ", " + this.margin_top + ")");
+      y$.attr('class', 'drawing');
+      z$ = this.datapaths = this.drawing.append('g');
+      z$.attr('class', 'datapaths');
       this.min_date = Math.min.apply(Math, this.lines.map(function(it){
         return it.datapoints[0].date.getTime();
       }));
       this.max_date = Math.max.apply(Math, this.lines.map(function(it){
         return it.datapoints[it.datapoints.length - 1].date.getTime();
       }));
-      z$ = this.scale_x = d3.scale.linear();
-      z$.domain([this.min_date, this.max_date]);
-      z$.range([0, this.width]);
-      z1$ = this.scale_y = d3.scale.linear();
-      z1$.domain([0, 100]);
-      z1$.range([this.height, 0]);
+      z1$ = this.scale_x = d3.scale.linear();
+      z1$.domain([this.min_date, this.max_date]);
+      z1$.range([0, this.width]);
+      z2$ = this.scale_y = d3.scale.linear();
+      z2$.domain([0, 100]);
+      z2$.range([this.height, 0]);
+      z3$ = this.line = d3.svg.line();
+      z3$.x(function(it){
+        return this$.scale_x(it.date.getTime());
+      });
+      z3$.y(function(it){
+        return this$.scale_y(it.percent);
+      });
+      this.draw();
     }
+    prototype.draw = function(){
+      var x$, y$, this$ = this;
+      x$ = this.datapaths.selectAll('path').data(this.lines).enter();
+      y$ = x$.append('path');
+      y$.attr('d', function(line){
+        return this$.line(line.datapoints);
+      });
+      return x$;
+    };
     return Graph;
   }());
 }).call(this);
--- www/ls/Graph.ls
+++ www/ls/Graph.ls
@@ -9,6 +9,9 @@ window.Graph = class Graph
             ..attr \width  @width
         @drawing = @svg.append \g
             ..attr \transform "translate(#{@margin_left}, #{@margin_top})"
+            ..attr \class \drawing
+        @datapaths = @drawing.append \g
+            ..attr \class \datapaths
 
         @min_date = Math.min ...@lines.map ->
              it.datapoints[0].date.getTime!
@@ -20,4 +23,15 @@ window.Graph = class Graph
         @scale_y = d3.scale.linear!
             ..domain [0 100]
             ..range [@height, 0]
+        @line = d3.svg.line!
+            ..x ~> @scale_x it.date.getTime!
+            ..y ~> @scale_y it.percent
+        @draw!
 
+    draw: ->
+        @datapaths.selectAll \path
+            .data @lines
+            .enter!
+                ..append \path
+                    ..attr \d (line) ~>
+                        @line line.datapoints
